f6bead972cecc0919376acd066ced2954b21b640
Krishna <krishnakulkarni827@gmail.com>
2024-07-28 23:38:19 +0530

    updated resilience4j fault tolerance features for user-service

--- user-service/src/main/java/com/example/user_service/controller/UserController.java
+++ user-service/src/main/java/com/example/user_service/controller/UserController.java
@@ -3,11 +3,19 @@ package com.example.user_service.controller;
 
 import com.example.user_service.model.User;
 import com.example.user_service.serivce.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/user")
@@ -16,7 +24,15 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/{id}")
-    public User getUser(@PathVariable Long id){
-        return userService.getUserDetails(id);
+    @CircuitBreaker(name = "userservice",fallbackMethod = "UserServiceFallback")
+    @TimeLimiter(name = "userservice") //returns CompletableFuture
+    @Retry(name = "userservice")
+    public CompletableFuture<ResponseEntity<User>> getUser(@PathVariable Long id){
+        return CompletableFuture.supplyAsync(()->new ResponseEntity<>(userService.getUserDetails(id), HttpStatus.OK));
+    }
+
+    // fallback for above api endpoint
+    public CompletableFuture<ResponseEntity<User>> UserServiceFallback(Long id, RuntimeException runtimeException){
+        return CompletableFuture.supplyAsync(()->new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR));
     }
 }
